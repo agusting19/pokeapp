ea7e7a9cad92699da986ac06bf8a4b2e3720a595
Agustin Gomez <agusleongomez@gmail.com>
2024-04-13 15:44:26 -0300

    update header component with i18n support and theme button

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,24 +1,30 @@
+import { Locale } from "@/i18n/config"
+import { getDictionary } from "@/i18n/getDictionary"
 import Image from "next/image"
+import LangButton from "./langButton"
+import ThemeButton from "./themeButton"
 
-// TODO: add dark mode and i18n
-const Header = () => {
+interface HeaderProps {
+  lang: Locale
+}
+
+const Header = async ({ lang }: HeaderProps) => {
+  const { components } = await getDictionary(lang)
+  const { header, themeButton } = components
+
   return (
     <header className="flex py-6 w-full items-center justify-between max-w-5xl m-auto px-6 xl:px-0">
       <div className="flex gap-2 items-center">
         <Image src="/pokeball.webp" alt="pokeball" width={35} height={35} />
-        <h1 className="font-bold text-2xl">PokeApp</h1>
+        <h1 className="font-bold text-2xl">{header.title}</h1>
       </div>
       <nav>
-        <ul className="flex text-zinc-700 text-lg font-medium gap-4">
+        <ul className="flex items-center text-zinc-700 text-lg font-medium gap-4">
           <li>
-            <a href="#" className="hover:bg-neutral-200 p-2 rounded-lg">
-              English
-            </a>
+            <ThemeButton lang={themeButton} />
           </li>
           <li>
-            <a href="#" className="hover:bg-neutral-200 p-2 rounded-lg">
-              Dark Mode
-            </a>
+            <LangButton lang={header.language} />
           </li>
         </ul>
       </nav>
