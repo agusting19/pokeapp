f3697081b536192189ab8efb8a1e9b27cef31f3e
Agustin Gomez <agusleongomez@gmail.com>
2024-04-13 15:55:59 -0300

    Update language button text in translations.json

--- frontend/src/components/langButton.tsx
+++ frontend/src/components/langButton.tsx
@@ -1,13 +1,21 @@
+"use client"
+
 import Link from "next/link"
+import { useSearchParams } from "next/navigation"
 
 interface LangButtonProps {
   lang: string
 }
 
 const LangButton = ({ lang }: LangButtonProps) => {
+  const searchParams = useSearchParams()
+  const currentTheme = searchParams.get("theme")
+
+  const language = lang === "En" ? "en" : "es"
+
   return (
     <Link
-      href={lang === "En" ? "/en" : "/es"}
+      href={`/${language}?theme=${currentTheme}`}
       className="hover:bg-neutral-200 p-2 rounded-lg"
     >
       {lang}
