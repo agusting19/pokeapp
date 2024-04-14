26b8701b70443074326b4ac4281f5ccb9dda60f0
Agustin Gomez <agusleongomez@gmail.com>
2024-04-14 12:18:01 -0300

    add swagger config

---
+++ backend/src/doc/swaggerConfig.ts
@@ -0,0 +1,18 @@
+import swaggerJSDoc from "swagger-jsdoc"
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "PokeApi",
+      version: "1.0.0",
+      description:
+        "Simple REST API to fetch Pokémon data from pokeapi.co made with Express.js and TypeScript.",
+    },
+  },
+  apis: ["./routes/*.ts"],
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+export default swaggerSpec
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,4 +5,5 @@ const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`)
+  console.log(`Swagger documentation: http://localhost:${PORT}/api-docs/`)
 })
