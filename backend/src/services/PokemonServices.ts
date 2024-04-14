a2631fa7c86bf85dee4a9365aef7f7dc5614d643
Agustin Gomez <agusleongomez@gmail.com>
2024-04-14 12:16:26 -0300

    add pokemonServices and types

---
+++ backend/src/services/PokemonServices.ts
@@ -0,0 +1,41 @@
+import { API_BASE_URL } from "../constants"
+import { PokemonApiResponse, PokemonInfo } from "../types/app"
+
+export const fetchPokemonList = async (limit: number, page: number) => {
+  const offset = limit * (page - 1)
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}?limit=${limit}&offset=${offset}`
+    )
+
+    if (!response.ok) {
+      throw new Error(
+        `API call failed with status ${response.status}: ${response.statusText}`
+      )
+    }
+
+    const data: PokemonApiResponse = await response.json()
+
+    return data.results
+  } catch (error) {
+    throw error
+  }
+}
+
+export const fetchPokemonInfo = async (id: string) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/${id}`)
+
+    if (!response.ok) {
+      throw new Error(
+        `API call failed with status ${response.status}: ${response.statusText}`
+      )
+    }
+
+    const data: PokemonInfo = await response.json()
+
+    return data
+  } catch (error) {
+    throw error
+  }
+}
