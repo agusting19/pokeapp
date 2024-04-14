0983698e3a55745cdbeb2c902af0a8a85f1065f4
Agustin Gomez <agusleongomez@gmail.com>
2024-04-14 12:32:48 -0300

    fix console warnings

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["raw.githubusercontent.com", "pokeapi.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pokeapi.co",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "**",
+      },
+    ],
   },
 }
 
