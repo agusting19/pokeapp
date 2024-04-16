bbf040f31431f87e2a2e5390b7f4c8d222491b9d
Agustin Gomez <agusleongomez@gmail.com>
2024-04-15 23:48:15 -0300

    include x-access-token header

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,31 +4,19 @@ import {
   PokemonUtilInfoResponse,
 } from "@/types/app"
 
+const SECRET_KEY = process.env.SECRET_KEY || "your_secret_key"
 const API_BASE_URL = "http://localhost:4000/api"
 export const POKEMON_URL = `${API_BASE_URL}/pokemon`
 export const POKEMON_INFO_URL = `${API_BASE_URL}/pokemonInfo`
 
-export const getAllPokemons = async () => {
-  try {
-    const response = await fetch(`${POKEMON_URL}?limit=10277&page=1`)
-
-    if (!response.ok) {
-      throw new Error(
-        `API call failed with status ${response.status}: ${response.statusText}`
-      )
-    }
-
-    const data: PokemonListResponse = await response.json()
-
-    return data.data
-  } catch (error) {
-    throw error
-  }
-}
-
 export const getPokemonList = async (url: string) => {
   try {
-    const response = await fetch(url)
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "x-access-token": SECRET_KEY,
+      },
+    })
 
     if (!response.ok) {
       throw new Error(
@@ -46,7 +34,12 @@ export const getPokemonList = async (url: string) => {
 
 export const getPokemonUtilInfo = async (url: string) => {
   try {
-    const response = await fetch(url)
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "x-access-token": SECRET_KEY,
+      },
+    })
 
     if (!response.ok) {
       throw new Error(
@@ -64,7 +57,12 @@ export const getPokemonUtilInfo = async (url: string) => {
 
 export const getPokemonInfo = async (url: string) => {
   try {
-    const response = await fetch(url)
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "x-access-token": SECRET_KEY,
+      },
+    })
 
     if (!response.ok) {
       throw new Error(
